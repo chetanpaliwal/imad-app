5d0843256f2281f983db412b472ad8297e6a5e66
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-10 12:07:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,8 +9,13 @@ element.innerHTML='new vlaue by the local code console.';
 //To move the image
 
 var img=document.getElementById('madi');
-
+var marginRight=0;
+function moveRight()
+{
+    marginRight=marginRight+10; 
+    img.style.marginRight=marginRight + 'px';
+}
 img.onclick = function()
 {
-  img.style.marginLeft = '100px';  
+    var interval = setInterval(moveRight,100);
 };
