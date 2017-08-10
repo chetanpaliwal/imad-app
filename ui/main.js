57291d7d0bbcafe50dedb644d147f818ca7869f2
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-10 11:52:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //To change the text
 
-var element = document.getElementById(main.text);
+var element = document.getElementById('main.text');
 
 element.innerHTML='new vlaue by the local code console.';
 
