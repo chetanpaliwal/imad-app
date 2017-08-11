261a20a08a31926d64ae64e0b144d60cbf1e87f0
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-11 23:09:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,6 +22,6 @@ button.onclick = function(){
         //Not done yet
   };
   //Make a request
-  request.open('GET','http://chetanpaliwalcool.imad.hausra-app.io/counter',true);
+  request.open('GET','http://chetanpaliwalcool.imad.hasura-app.io/counter',true);
   request.send(null);
 };
