1c63209fd803ffc678c8d45f7c13c0b7afa688c2
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-11 23:08:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
 // Counter Value
 
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
   
-  //Make a request to counter end point.
+  //Creating a request object.
+  var request = new XMLHttpRequest();
+  
   
   //Capture the response and store in a variable.
-  
-  //Render the varaible in the correct span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  request.onreadystatechange = function(){
+    
+    if(request.readystate === XMLHttpRequest.DONE){
+        //Take some action
+    
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }  
+        //Not done yet
+  };
+  //Make a request
+  request.open('GET','http://chetanpaliwalcool.imad.hausra-app.io/counter',true);
+  request.send(null);
 };
