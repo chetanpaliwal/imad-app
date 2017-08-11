a6f21515835606ac39cb82e21e0ed13f7865b01b
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-11 22:57:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,15 @@
-console.log('Loaded!');
+// Counter Value
 
-//To change the text
-
-var element = document.getElementById('main.text');
-
-element.innerHTML='new vlaue by the local code console.';
-
-//To move the image
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+1; 
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+  
+  //Make a request to counter end point.
+  
+  //Capture the response and store in a variable.
+  
+  //Render the varaible in the correct span
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
