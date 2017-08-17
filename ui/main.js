87647c49ccf4617fa6e0c131d08910e5f2f2c30a
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-17 11:44:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,8 +25,6 @@ button.onclick = function(){
 };
 
 //Submit the name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -57,7 +55,9 @@ submit.onclick = function(){
     //Not done yet
   };
   
-  //Make a request  
+  //Make a request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   request.open('GET','http://chetanpaliwalcool.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
     
