d0d422c05765fcee1e3c9fe69102a0d52292717a
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-17 12:08:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,14 @@ app.get('/article-one',function(req, res) {
   res.send(createTemplate(articleOne)); 
 });
 
+app.get('/article-two',function(req, res) {
+  res.sendFile(path.join(__dirname,'ui','article-two.html')); 
+});
+
+app.get('/article-three',function(req, res) {
+  res.sendFile(path.join(__dirname,'ui','article-three.html')); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
