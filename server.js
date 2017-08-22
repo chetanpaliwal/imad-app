f507ad23b688828ee92942b9bbe561fa3b1f938d
Chetan Paliwal <30632694+chetanpaliwalcool@users.noreply.github.com>
2017-08-22 14:01:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'chetanpaliwalcool',
@@ -52,6 +53,17 @@ function createTemplate(data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash (input, salt) {
+    //How to create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 1000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/has/:input',function(req, res) {
+    var hashedString = has(req.params.input, 'this-is-a-random-string');
+    res.send(hashedString);
 });
 
 var pool = new Pool(config);
